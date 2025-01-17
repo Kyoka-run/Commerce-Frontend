f3b86b99bba748fc8547382ac83ac06c098b3826
KyokaKyoka <752572249@qq.com>
2025-01-17 09:06:36 +0000

    impl product pagation

--- src/store/reducer/ProductReducer.js
+++ src/store/reducer/ProductReducer.js
@@ -12,15 +12,44 @@ export const productReducer = (state = initialState, action) => {
                 products: action.payload,
                 pagination: {
                     ...state.pagination,
-                    pageNumber: action.number,
-                    pageSize: action.size,
+                    pageNumber: action.pageNumber,
+                    pageSize: action.pageSize,
                     totalElements: action.totalElements,
                     totalPages: action.totalPages,
                     lastPage: action.lastPage,
                 },
             };
-            
-        default:   
+
+        case "FETCH_PRODUCTS":
+            return {
+                ...state,
+                products: action.payload,
+                pagination: {
+                    ...state.pagination,
+                    pageNumber: action.pageNumber,
+                    pageSize: action.pageSize,
+                    totalElements: action.totalElements,
+                    totalPages: action.totalPages,
+                    lastPage: action.lastPage,
+                },
+            };
+    
+        case "FETCH_CATEGORIES":
+            return {
+                ...state,
+                categories: action.payload,
+                pagination: {
+                    ...state.pagination,
+                    pageNumber: action.pageNumber,
+                    pageSize: action.pageSize,
+                    totalElements: action.totalElements,
+                    totalPages: action.totalPages,
+                    lastPage: action.lastPage,
+                },
+            };
+        
+    
+        default:
             return state;
     }
 };
